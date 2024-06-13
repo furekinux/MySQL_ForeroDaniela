6686a95b62baf41dfd81788aa8042e12598c999a
Daniela Forero Ballén <156367697+furekinux@users.noreply.github.com>
2024-06-13 14:13:44 +0000

    feat: :sparkles: Subir actividad tipos de JOIN

--- dia_04/ejercicio_1.sql
+++ dia_04/ejercicio_1.sql
@@ -1,6 +1,6 @@
--- #####################
--- ### EJERCICIO # 1 ###
--- #####################
+-- #############################################
+-- ### DIA #4 - RELACIONAMIENTO DE CONSULTAS ###
+-- #############################################
 
 -- CREAR Y USAR BBDD
 CREATE database dia_04;
@@ -39,5 +39,42 @@ es_oficial tinyint(1)
 );
 
 show tables;
+
+-- ########### CONSULTAS ############
+
+-- Todos los pares de nombres de paises y sus ciudades
+-- correspondientes que están relacionadas
+-- en la base de datos (INNER JOIN = COINCIDENCIA EN
+-- AMBAS TABLAS, como la intersección de conjuntos).
+SELECT pais.nombre as Pais,
+ciudad.nombre as Ciudad
+from pais -- Pais es un conjunto A
+inner join ciudad -- Ciudad es un conjunto B
+on pais.id = ciudad.id_pais; -- Interseccion de A y B
+
+-- ######### INGRESO ADICIONAL #########
+INSERT INTO ciudad VALUES
+(7, 'Ciudad Unknow', Null);
+INSERT INTO pais VALUES
+(4, 'Italia','Europa', 10000022);
+
+-- Listar todas las ciudades con el nombre de su pais
+-- si alguna ciudad no tiene pais asignado, aun aparecera
+-- en la lista con null (LEFT JOIN)
+SELECT pais.nombre as Pais,
+ciudad.nombre as Ciudad
+from pais -- Pais es un conjunto A
+left join ciudad -- Ciudad es un conjunto B
+on pais.id = ciudad.id_pais; -- Interseccion de A y B
+
+-- Mostrar todos los paises y si tienen ciudades asociadas,
+-- se muestran junto al nombre del pais. Si no hay ciudades
+-- asociadas a un pais, el nombre de la ciudad aparecera
+-- como null (RIGHT JOIN)
+SELECT pais.nombre as Pais,
+ciudad.nombre as Ciudad
+from pais -- Pais es un conjunto A
+right join ciudad -- Ciudad es un conjunto B
+on pais.id = ciudad.id_pais; -- Interseccion de A y B
 
 -- Desarrollado por Daniela Forero / ID.1.142.714.225
